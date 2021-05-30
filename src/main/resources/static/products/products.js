c7138f188395769e2b5c8a23b1ece344448371c4
IDA <isakov.danila.79@gmail.com>
2021-05-30 23:28:49 +0200

    AOP services timer statistics, user orders displays in separate page

--- src/main/resources/static/products/products.js
+++ src/main/resources/static/products/products.js
@@ -1,14 +1,6 @@
 angular.module('app').controller('productsController', function ($scope, $http, $localStorage) {
     const contextPath = 'http://localhost:8189/market';
 
-    $scope.isUserLoggedIn = function () {
-        if ($localStorage.aprilMarketCurrentUser) {
-            return true;
-        } else {
-            return false;
-        }
-    };
-
     $scope.loadPage = function (page) {
         $http({
             url: contextPath + '/api/v1/products',
@@ -33,6 +25,15 @@ angular.module('app').controller('productsController', function ($scope, $http, 
         });
     };
 
+    $scope.loadCart = function (page) {
+        $http({
+            url: contextPath + '/api/v1/cart',
+            method: 'GET'
+        }).then(function (response) {
+            $scope.cartDto = response.data;
+        });
+    };
+
     $scope.addToCart = function (productId) {
         $http({
             url: contextPath + '/api/v1/cart/add/' + productId,
@@ -48,19 +49,6 @@ angular.module('app').controller('productsController', function ($scope, $http, 
             arr.push(i);
         }
         return arr;
-    }
-
-    $scope.showMyOrders = function () {
-        $http({
-            url: contextPath + '/api/v1/orders',
-            method: 'GET'
-        }).then(function (response) {
-            $scope.myOrders = response.data;
-        });
-    };
-
-    if ($scope.isUserLoggedIn()) {
-        $scope.showMyOrders();
     }
 
     $scope.loadPage(1);
